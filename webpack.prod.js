00853e7b3ccb294dfe70e59789c67afda650f027
cieloazul310 <cieloazul310@gmail.com>
2017-09-04 21:55:48 +0900

    babelify scripts

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,27 @@
 const webpack = require("webpack");
-const merge = require("webpack-merge");/*
-const UglifyJSPlugin = require("uglifyjs-webpack-plugin");*/
-const MinifyPlugin = require("babel-minify-webpack-plugin");
+const merge = require("webpack-merge");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   devtool: "cheap-module-source-map",
-  plugins: [/*
-    new UglifyJSPlugin(),*/
-    new MinifyPlugin(),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+            plugins: ['syntax-dynamic-import']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new UglifyJSPlugin(),
     new webpack.DefinePlugin({
       "process.env": {
         "NODE_ENV": JSON.stringify("production")
